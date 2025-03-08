814b5b0e7280c5db98e605f2829268125e1e5213
Mohnish Gupta <mohnishgupta12@gmail.com>
2025-03-08 13:43:56 +0530

    removed api key

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ class Config:
         EMBEDDING_MODEL_NAME (str): Name of the embedding model used for text vectorization.
         DOCUMENTS_PATH (str): Path to the directory containing documents to be processed.
     """
-    OPENAI_API_KEY= "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"
+    OPENAI_API_KEY= "Provide OPEN_AI_KEY"
     CHROMA_COLLECTION_NAME = "abinbev_docs_v2"
     EMBEDDING_MODEL_NAME = "all-MiniLM-L6-v2"
     DOCUMENTS_PATH = "app/ubuntu-docs/"
